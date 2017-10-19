78187fa398bf00adeb0c212d7dd1385d25ae09a2
Chris Wells <chris@nexcess.net>
2017-10-18 22:39:13 -0400

    testing

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -xe
 
 if [ "$1" = "7" ]; then
-    docker pull geerlingguy/docker-centos7-ansible:latest
     docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro geerlingguy/docker-centos7-ansible:latest /usr/lib/systemd/systemd --volume=`pwd`:/etc/ansible:ro
 
     ID=$(docker ps | grep centos | awk '{print $1}')
 
     docker logs $ID
     docker exec --tty $ID env TERM=xterm ansible --version
-    docker exec --tty $ID env TERM=xterm ansible-playbook /path/to/ansible/playbook.yml --syntax-check
+    docker exec --tty $ID env TERM=xterm playbook setup
 fi
