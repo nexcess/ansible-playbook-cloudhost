dbf376f4985911a6eb84237b9d8382688c00c185
Chris Orlando <chrsorlando@gmail.com>
2018-11-23 16:40:18 -0500

    remove php tests

--- spec/default/cloudhost_php_spec.rb
+++ spec/default/cloudhost_php_spec.rb
@@ -4,25 +4,10 @@ describe package('php56') do
   it { should be_installed }
 end
 
-describe service('php56-php-fpm') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe package('php70') do
   it { should be_installed }
 end
 
-describe service('php70-php-fpm') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
 describe package('php71') do
   it { should be_installed }
-end
-
-describe service('php71-php-fpm') do
-  it { should be_enabled }
-  it { should be_running }
 end
